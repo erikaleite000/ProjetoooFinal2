9cebba7ab4b1e46e036a94e0fe195d9ec6f5b12a
nataliaferraz <nataliamoreira186@gmail.com>
2025-01-29 11:01:54 -0400

    nat

---
+++ src/main/resources/Main.java
@@ -0,0 +1,11 @@
+import repository.LivroRepository;
+import view.CadastroDeLivro;
+import view.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        new CadastroDeLivro();
+        new Principal();
+        new LivroRepository();
+    }
+}
